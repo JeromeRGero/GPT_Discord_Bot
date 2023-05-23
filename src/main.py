7c3052df0d32883b80cca13269d8e477d2a05548
Jerome Gero <JeromeRGero@gmail.com>
2023-05-23 19:17:43 -0400

    updating main py to use process_thread_message

--- src/main.py
+++ src/main.py
@@ -39,51 +39,8 @@ async def on_thread_create(thread: Thread):
     # Process first message (somehow)
     message_reference = thread.starter_message
     id_conversation_name = f'thread_{thread.id}_{thread.name}'
-    await process_conversation(message_reference.content, id_conversation_name, message_reference)
-    # if message_reference:
-    #     start_message = await thread.fetch_message(message_reference.message_id)
-    #     async for message in thread.history(oldest_first=True, limit=1):
-    #          first_message = message
-    #          break
-    #     print(f'Thread created: {thread.name}')
-    #     print(f'First message: {message_reference.content}')
-    #     await thread.send('Hello, I am a bot. \
-    #                       I will be assisting you in this conversation, someday.')
+    await process_thread_message(message_reference.content, id_conversation_name, thread)
     return
-
-# @bot.event
-# async def on_message(message: Message):
-#     # Ignore messages from the bot itself and messages from other channels.
-#     if message.author == bot.user or message.channel.name != 'gpt-4-deus':
-#         return
-
-#     # Process commands
-#     content: str = message.content
-#     if content.startswith('?'):
-#         await bot.process_commands(message)
-#         return
-
-#     # Continue or create a conversation.
-#     userid = message.author.id
-#     username = message.author.name
-#     print(f'User: {username}\n {content}')
-
-#     user_conversations_document = await get_user_conversation_list(userid)
-#     if user_conversations_document is None or \
-#         user_conversations_document['active_conversation'] is None or \
-#             user_conversations_document['active_conversation'] == '':
-#         await message.channel.send(
-#             'Please create a new conversation instance using the `?new` command. \
-#                 or jump to an existing conversation using the `?jumpto` command. \
-#                     You can list all your conversations using the `?list` command.'
-#         )
-#         return
-    
-#     # Get the active conversation. formated as userid_conversationname
-#     id_conversation_name = str(user_conversations_document['active_conversation'])
-#     print(f'Active conversation found: {id_conversation_name}')
-
-#     await process_conversation(content, id_conversation_name, message)
 
 # TODO: finish working on the threads functionality.
 @bot.event
