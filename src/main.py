1d6bac502db2ae0f8bfa05f0b57ca591eef20a79
Wynnreynel <101367720+Wynnreynel@users.noreply.github.com>
2023-05-22 21:13:28 -0400

    Thread
    Added thread thing funny hahahahahahaha

--- src/main.py
+++ src/main.py
@@ -33,19 +33,22 @@ async def on_ready():
 @bot.event
 async def on_thread_create(thread: Thread):
     parent_channel_name = thread.parent.name
-    if parent_channel_name != 'gpt-4-deus':
+
+    if thread.owner_id != bot.user.id or parent_channel_name != 'gpt-prime':
         return
     # Process first message (somehow)
     message_reference = thread.starter_message
-    if message_reference:
-        # start_message = await thread.fetch_message(message_reference.message_id)
-        # async for message in thread.history(oldest_first=True, limit=1):
-        #     first_message = message
-        #     break
-        print(f'Thread created: {thread.name}')
-        print(f'First message: {message_reference.content}')
-        await thread.send('Hello, I am a bot. \
-                          I will be assisting you in this conversation, someday.')
+    id_conversation_name = f'thread_{thread.id}_{thread.name}'
+    await process_conversation(message_reference.content, id_conversation_name, message_reference)
+    # if message_reference:
+    #     start_message = await thread.fetch_message(message_reference.message_id)
+    #     async for message in thread.history(oldest_first=True, limit=1):
+    #          first_message = message
+    #          break
+    #     print(f'Thread created: {thread.name}')
+    #     print(f'First message: {message_reference.content}')
+    #     await thread.send('Hello, I am a bot. \
+    #                       I will be assisting you in this conversation, someday.')
     return
 
 # @bot.event
@@ -89,32 +92,28 @@ async def on_command_error(content, exception):
 
 @bot.event
 async def on_message(message: Message):
+    #If message is in a thread
     if isinstance(message.channel, discord.threads.Thread):
-        # thread_channel_name: str = message.channel.name
+        thread_channel_name: str = message.channel.name
+        thread_channel_id = message.channel.id
         parent_channel_name = message.channel.parent.name
-        if message.author == bot.user or parent_channel_name != 'gpt-4-deus':
+        #If message is in the correct channel
+        if message.author == bot.user or parent_channel_name != 'gpt-prime':
             return
-        # Process text in thread.
+        await process_conversation(message.content, f'thread_{message.channel.id}_{message.channel.name}', message)
+        return
+        # Process commands in the thread
     else:
-        # Create a new thread.
+        # Ignore messages from the bot itself and messages from other channels.
+        if message.author == bot.user or message.channel.name != 'gpt-prime':
+            return
+        # Process commands
         content: str = message.content
-        # id_conversation_name: str = get_id_conversation(ctx.author.id, content[0:10])
-        # print(f"Creating a new thread for the new conversation for user [{ctx.author.name}: {ctx.author.id}]")
-        # await store_in_user_conversation_list(ctx.author.id, username, name)
-        # await ctx.send(f'Created a new conversation instance with name `{name}`')
-        thread: Thread = await message.create_thread(name=content[:10])
-        print(f'# Does this every hit?  \nCreated a new thread: {thread.name}')
-    return
-    # Ignore messages from the bot itself and messages from other channels.
-    if message.author == bot.user or message.channel.name != 'gpt-prime':
-        return
+        print(f'Content: {content}')
+        if content.startswith('!'):
+            await bot.process_commands(message)
+            return
 
-    # Process commands
-    content: str = message.content
-    print(f'Content: {content}')
-    if content.startswith('!'):
-        await bot.process_commands(message)
-        return
 
     #TODO Create thread if no active conversation
 
@@ -127,11 +126,12 @@ async def on_message(message: Message):
     if user_conversations_document is None or \
         user_conversations_document['active_conversation'] is None or \
             user_conversations_document['active_conversation'] == '':
-        await message.channel.send(
-"""Please create a new conversation instance using the `?new` command.
-or jump to an existing conversation using the `?jumpto` command.
-You can list all your conversations using the `?list` command."""
-        )
+        # Create a new thread.
+        convoName: str = content[0:10]
+        id_conversation_name: str = get_id_conversation(message.author.id, convoName)
+        print(f"Creating a new thread for the new conversation for user [{message.author.name}: {message.author.id}]")
+        thread: Thread = await message.create_thread(name=convoName)
+        print(f'# Does this every hit?  \nCreated a new thread: {thread.name}')
         return
     
     # Get the active conversation. formated as userid_conversationname
